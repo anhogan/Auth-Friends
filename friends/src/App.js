c46a1366e569511ee70a0463bf73c215d9572664
Amanda Hogan <ahogan@teamworks.com>
2020-02-25 11:27:02 -0500

    Setup PrivateRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react';
+import { Route, Link, Redirect } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import FriendList from './components/FriendsList';
 import './App.css';
 
 function App() {
+  const PrivateRoute = ({ component: FriendList, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <FriendList {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+
   return (
     <div className="App">
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/friends">Friends</Link>
+      </nav>
       <Route path="/login" component={LoginPage} />
+      <PrivateRoute path="/friends" component={FriendList} />
     </div>
   );
 }
