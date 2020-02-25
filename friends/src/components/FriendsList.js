436c346f1b92f48fdd862d84148625c29d5ac28e
Amanda Hogan <ahogan@teamworks.com>
2020-02-25 11:47:26 -0500

    Map over friends list

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const FriendList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/friends')
+      .then(res => {
+        console.log(res);
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
+
   return (
     <div>
-      
+      {friends.map((friend) => (
+        <div key={friend.id}>
+          <h2>{friends.name}</h2>
+          <p>Age: {friend.age}</p>
+          <p>Email: {friend.email}</p>
+        </div>
+      ))}
     </div>
   );
 };
