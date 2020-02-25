3d507a812cf9afbedcb5da346bca12485f158bca
Amanda Hogan <ahogan@teamworks.com>
2020-02-25 11:07:50 -0500

    Setup login form

--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsername('');
+    setPassword('');
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
-
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username" />
+          <input id="username" name="username" type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label htmlFor="password" />
+          <input id="password" name="password" type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button>Login</button>
+      </form>
     </div>
   );
 };
